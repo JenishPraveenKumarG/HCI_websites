680667ac3c49755a9c8176cc9e3366ef5a51403e
Jenish Praveen Kumar G <137860558+JenishPraveenKumarG@users.noreply.github.com>
2024-05-24 21:07:33 +0530

    Add files via upload

---
+++ HCI_5/script.js
@@ -0,0 +1,5 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Registration successful!');
+    // Here you can handle form submission, e.g., send data to a server
+});
