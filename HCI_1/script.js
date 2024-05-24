fda5ace7cccb543372402cc0cd930d199e2dccd1
Jenish Praveen Kumar G <137860558+JenishPraveenKumarG@users.noreply.github.com>
2024-05-24 20:33:54 +0530

    Add files via upload

---
+++ HCI_1/script.js
@@ -0,0 +1,4 @@
+document.getElementById('enterButton').addEventListener('click', function() {
+    alert('Welcome! You have entered the site.');
+    // Here you can add the code to redirect to another page or perform any other action
+});
